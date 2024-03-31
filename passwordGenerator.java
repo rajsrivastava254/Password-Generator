b09874a360ad9f29b4eeed2ec430f931a9b7828d
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-03-31 14:25:24 +0530

    passwordGenerator.java

--- passwordGenerator.java
+++ passwordGenerator.java
@@ -1,39 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.security.SecureRandom;
-import java.util.Scanner;
 
-public class PasswordGenerator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class PasswordGenerator extends JFrame {
+    private JCheckBox upperCaseCheckBox;
+    private JCheckBox lowerCaseCheckBox;
+    private JCheckBox numbersCheckBox;
+    private JCheckBox specialCharsCheckBox;
+    private JTextField lengthField;
+    private JTextArea passwordArea;
 
-        System.out.print("Enter the desired password length: ");
-        int length = scanner.nextInt();
-        scanner.nextLine();  // Consume the newline character
+    public PasswordGenerator() {
+        setTitle("Password Generator");
+        setSize(400, 300);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        System.out.print("Include uppercase letters? (yes/no): ");
-        boolean includeUpperCase = scanner.nextLine().equalsIgnoreCase("yes");
+        add(new JLabel("Password Length:"));
+        lengthField = new JTextField(10);
+        add(lengthField);
 
-        System.out.print("Include lowercase letters? (yes/no): ");
-        boolean includeLowerCase = scanner.nextLine().equalsIgnoreCase("yes");
+        upperCaseCheckBox = new JCheckBox("Include Uppercase Letters");
+        lowerCaseCheckBox = new JCheckBox("Include Lowercase Letters");
+        numbersCheckBox = new JCheckBox("Include Numbers");
+        specialCharsCheckBox = new JCheckBox("Include Special Characters");
 
-        System.out.print("Include numbers? (yes/no): ");
-        boolean includeNumbers = scanner.nextLine().equalsIgnoreCase("yes");
+        add(upperCaseCheckBox);
+        add(lowerCaseCheckBox);
+        add(numbersCheckBox);
+        add(specialCharsCheckBox);
 
-        System.out.print("Include special characters? (yes/no): ");
-        boolean includeSpecialChars = scanner.nextLine().equalsIgnoreCase("yes");
+        JButton generateButton = new JButton("Generate Password");
+        generateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generatePassword();
+            }
+        });
+        add(generateButton);
 
-        String password = generatePassword(length, includeUpperCase, includeLowerCase, includeNumbers, includeSpecialChars);
-        System.out.println("Generated Password: " + password);
+        passwordArea = new JTextArea(5, 20);
+        passwordArea.setEditable(false);
+        add(new JScrollPane(passwordArea));
 
-        scanner.close();
+        setVisible(true);
     }
 
+    private void generatePassword() {
+        int length = Integer.parseInt(lengthField.getText());
+        boolean includeUpperCase = upperCaseCheckBox.isSelected();
+        boolean includeLowerCase = lowerCaseCheckBox.isSelected();
+        boolean includeNumbers = numbersCheckBox.isSelected();
+        boolean includeSpecialChars = specialCharsCheckBox.isSelected();
+
+        String password = PasswordGenerator1.generatePassword(length, includeUpperCase, includeLowerCase, includeNumbers, includeSpecialChars);
+        passwordArea.setText(password);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new PasswordGenerator();
+            }
+        });
+    }
+}
+
+class PasswordGenerator1 {
     public static String generatePassword(int length, boolean includeUpperCase, boolean includeLowerCase, boolean includeNumbers, boolean includeSpecialChars) {
         String uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
         String numberChars = "0123456789";
         String specialChars = "!@#$%^&*()-_+=<>?";
-
         StringBuilder validChars = new StringBuilder();
+        SecureRandom random = new SecureRandom();
 
         if (includeUpperCase) {
             validChars.append(uppercaseChars);
@@ -48,15 +91,13 @@ public class PasswordGenerator {
             validChars.append(specialChars);
         }
 
-        SecureRandom random = new SecureRandom();
         StringBuilder password = new StringBuilder();
-
         for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(validChars.length());
-            char randomChar = validChars.charAt(randomIndex);
-            password.append(randomChar);
+            int index = random.nextInt(validChars.length());
+            password.append(validChars.charAt(index));
         }
 
         return password.toString();
     }
 }
+
