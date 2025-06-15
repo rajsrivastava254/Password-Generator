20b00e1812258ed78eebccbb474d9bf3db4842ca
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2025-06-15 21:12:38 +0530

    Delete sum.java

--- sum.java
+++
@@ -1,9 +0,0 @@
-public class sum{
-    public static void main(String[] args){
-        int a = 10;
-        int b = 5;
-        System.out.println(a+b);
-        int c=a+b+b;
-        System.out.println(c);
-    }
-}
